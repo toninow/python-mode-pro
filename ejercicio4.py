5dfb40ac1f71f403e120ff83e43d42cc3bc58583
toninow <abenalcazarc@gmail.com>
2022-05-09 10:42:27 -0500

    ejercicio promedio

---
+++ ejercicio4.py
@@ -0,0 +1,16 @@
+#Se necesita obtener el promedio simple de un estudiante a partir
+#de sus tres notas parciales N1, N2 y N3.
+
+print("++++++++++++++++++++++++++++++++++++++++")
+print("promedio estudiante")
+print("++++++++++++++++++++++++++++++++++++++++")
+
+n1 = float(input("Ingrese su primera nota: "))
+n2 = float(input ("Ingrese su segunda nota: "))
+n3 = float(input("Ingrese su tercera nota: "))
+
+promedio = float((n1+n2+n3)/3)
+print("Su promedio es:")
+print(float(promedio))
+
+
